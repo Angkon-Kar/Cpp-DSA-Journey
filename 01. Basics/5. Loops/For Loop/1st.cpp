0739c9996557b069d1a8893ada579bdff01c9af0
Angkon Kar <angkon310@gmail.com>
2025-05-04 01:11:16 +0600

    For Loop = added four codes file.

---
+++ 01. Basics/5. Loops/For Loop/1st.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << "Hello!!" << endl;
+    }
+    return 0;
+}
+
+/*
+    Output = 
+    Hello!!
+    Hello!!
+    Hello!!
+    Hello!!
+    Hello!!
+    Hello!!
+    Hello!!
+    Hello!!
+    Hello!!
+    Hello!!
+*/
---
+++ 01. Basics/5. Loops/For Loop/3rd.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
+
+/*
+    Output = 
+    1
+    2
+    3
+    4
+    5
+    6
+    7
+    8
+    9
+    10
+*/
