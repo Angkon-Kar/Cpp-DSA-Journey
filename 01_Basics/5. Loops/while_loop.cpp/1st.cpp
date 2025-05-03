111220b4ad51ba41c3de8fa72e6329b6e2865f48
Angkon Kar <angkon310@gmail.com>
2025-05-03 23:53:14 +0600

    Adding Loops file & some while loop codes

---
+++ 01_Basics/5. Loops/while_loop.cpp/1st.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int count = 1;
+    while (count <= 10)
+    {
+        cout << "Hello!!" << endl;
+        count = count + 1;
+    }
+    return 0;
+}
+
+/*  
+    Output=
+
+    Hello!!
+    Hello!!
+    Hello!!
+    Hello!!
+    Hello!!
+    Hello!!
+    Hello!!
+    Hello!!
+    Hello!!
+    Hello!!
+*/
