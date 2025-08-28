3624b2e3b6fbe78d5de38a49de104a7fca1a4368
Angkon Kar <angkon310@gmail.com>
2025-08-28 18:31:39 +0600

    STL Update

---
+++ 02. STL/Basics/06_removing_elements.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> numbers = {10, 20, 30, 40, 50};
+    
+    // Remove the last element.
+    numbers.pop_back();
+
+    // Remove the first element using an iterator.
+    numbers.erase(numbers.begin());
+    
+    cout << "Elements after removal: ";
+    for (int num : numbers) {
+        cout << num << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
