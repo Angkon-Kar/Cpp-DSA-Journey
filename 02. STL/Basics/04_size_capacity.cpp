82389bf7bf2e7bce4dd084dba31083fd81139f6f
Angkon Kar <angkon310@gmail.com>
2025-08-28 18:17:27 +0600

    STL Update

---
+++ 02. STL/Basics/04_size_capacity.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> numbers = {10, 20, 30};
+
+    // Get the number of elements.
+    cout << "Number of elements: " << numbers.size() << endl;
+    
+    // Get the total space allocated.
+    cout << "Current capacity: " << numbers.capacity() << endl;
+    
+    numbers.push_back(40);
+    cout << "\nAfter adding an element..." << endl;
+    cout << "New size: " << numbers.size() << endl;
+    cout << "New capacity: " << numbers.capacity() << endl;
+    
+    return 0;
+}
