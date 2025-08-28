bd75770d45ca18e216a2c47f14157e41a632b261
Angkon Kar <angkon310@gmail.com>
2025-08-28 18:27:20 +0600

    STL Update

---
+++ 02. STL/Basics/05_iterating.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> numbers = {10, 20, 30, 40, 50};
+
+    // Iterate using a range-based for loop.
+    cout << "Elements (range-based loop): ";
+    for (int num : numbers) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    // Iterate using a traditional for loop with an index.
+    cout << "Elements (indexed loop): ";
+    for (size_t i = 0; i < numbers.size(); ++i) {
+        cout << numbers[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
