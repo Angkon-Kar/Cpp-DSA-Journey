07f00df5e843d923e66fa88fb2da9fc85e85708a
Angkon Kar <angkon310@gmail.com>
2025-11-12 22:41:16 +0600

    Added 02_Generic_Programming_Why_Use_STL.cpp

---
+++ 02. STL/All About STL/03_STL_Features_Containers_Iterators_Algorithms.cpp
@@ -0,0 +1,19 @@
+//  03_STL_Features_Containers_Iterators_Algorithms.cpp
+
+// The C++ Standard Template Library
+// Douglas C. Schmidt
+// Vanderbilt University
+// February 12, 2014
+//
+// STL Features: Containers, Iterators, & Algorithms
+// • Containers
+//   - Sequential: vector, deque, list
+//   - Associative: set, multiset, map, multimap
+//   - Adapters: stack, queue, priority_queue
+// • Iterators
+//   - Input, output, forward, bidirectional, & random access
+//   - Each container declares a trait for the type of iterator it provides
+// • Generic Algorithms
+//   - Mutating, non-mutating, sorting, & numeric
+
+// No executable code for this conceptual file.
