5d1899195e4b3b984b1d131b5c54668ab12bf0f0
Angkon Kar <angkon310@gmail.com>
2025-11-12 22:38:42 +0600

    Added 01_what_is_STL.cpp

---
+++ 02. STL/All About STL/01_what_is_STL.cpp
@@ -0,0 +1,23 @@
+//  What is STL?
+
+// The C++ Standard Template Library
+// Douglas C. Schmidt
+// Vanderbilt University
+// February 12, 2014
+//
+// What is STL?
+// The Standard Template Library provides a set of well-structured generic C++ components that work together in a seamless way.
+// - Alexander Stepanov & Meng Lee, The Standard Template Library
+//
+// What is STL (cont'd)?
+// • A collection of composable class & function templates
+//   - Helper class & function templates: operators, pair
+//   - Container & iterator class templates
+//   - Generic algorithms that operate over iterators
+//   - Function objects
+//   - Adaptors
+// • Enables generic programming in C++
+//   - Each generic algorithm can operate over any iterator for which the necessary operations are provided
+//   - Extensible: can support new algorithms, containers, iterators
+
+// No executable code for this introductory file.
