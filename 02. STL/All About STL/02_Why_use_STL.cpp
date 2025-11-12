a4cc063b82d02acd3030458cd10bb7a4a456b703
Angkon Kar <angkon310@gmail.com>
2025-11-12 22:40:29 +0600

    Added 02_Generic_Programming_Why_Use_STL.cpp

---
+++ 02. STL/All About STL/02_Why_use_STL.cpp
@@ -0,0 +1,20 @@
+//  02_Generic_Programming_Why_Use_STL.cpp
+
+// The C++ Standard Template Library
+// Douglas C. Schmidt
+// Vanderbilt University
+// February 12, 2014
+//
+// Generic Programming: Why Use STL?
+// • Reuse: "write less, do more"
+//   - STL hides complex, tedious & error-prone details
+//   - The programmer can then focus on the problem at hand
+//   - Type-safe plug compatibility between STL components
+// • Flexibility
+//   - Iterators decouple algorithms from containers
+//   - Unanticipated combinations easily supported
+// • Efficiency
+//   - Templates avoid virtual function overhead
+//   - Strict attention to time complexity of algorithms
+
+// No executable code for this conceptual file.
