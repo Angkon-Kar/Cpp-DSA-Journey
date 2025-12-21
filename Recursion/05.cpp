9632fdefd14809e4acdc54bf74b933456c9ce35f
Angkon Kar <angkon310@gmail.com>
2025-12-21 20:51:41 +0600

    Recursion problem

--- Recursion/05.cpp
+++ Recursion/05.cpp
@@ -1,1 +1,21 @@
 // Given an integer n(1 <= n <= 100000), print the digits of n in actual order using recursion.
+#include <bits/stdc++.h>
+using namespace std;
+
+void func(int n){
+	//base case
+	if(n==0) return;
+	
+	//function body
+	int last_digit = n%10;
+	n/=10;
+	func(n);
+	
+	cout<<last_digit<<" ";
+}
+
+int main() {
+	func(3648);
+	//3 6 4 8
+	return 0;
+}
