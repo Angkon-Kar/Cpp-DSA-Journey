199e553254199b6f32474069f610ea6fd22a9efc
Angkon Kar <angkon310@gmail.com>
2025-12-21 20:45:10 +0600

    Recursion problem

--- Recursion/03.cpp
+++ Recursion/03.cpp
@@ -1,46 +1,20 @@
-// Given an integer n(1 <= n <= 100000), print the digits of n in reverse order using recursion.
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void printReverse(int n) {
-    // base case
-    if (n < 10) {
-        cout << n << " ";
-        return;
-    }
-    
-    // print last digit
-    cout << n % 10 << " ";
-    
-    // recursive call with remaining digits
-    printReverse(n / 10);
+void func(int n){
+	//base case
+	if(n==0) return;
+	
+	//function body
+	int last_digit = n%10;
+	cout<<last_digit<<" ";
+	n/=10;
+	func(n);
+	
 }
 
 int main() {
-    int n;
-    cout << "Enter an integer (1 <= n <= 100000): ";
-    cin >> n;
-    
-    if (n < 1 || n > 100000) {
-        cout << "Input out of range!" << endl;
-        return 1;
-    }
-    
-    cout << "Digits in reverse order: ";
-    printReverse(n);
-    cout << endl;
-    
-    return 0;
+	func(3648);
+	//8 4 6 3
+	return 0;
 }
-
-/*
-output Example:
-Enter an integer (1 <= n <= 100000): 12345
-Digits in reverse order: 5 4 3 2 1
-
-Enter an integer (1 <= n <= 100000): 789
-Digits in reverse order: 9 8 7
-
-Enter an integer (1 <= n <= 100000): 52479
-Digits in reverse order: 9 7 4 2 5 
-*/
