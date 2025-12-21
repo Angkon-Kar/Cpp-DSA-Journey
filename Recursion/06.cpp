52b2f6d1c0d3bcaabee8275e85eb80605eb3faeb
Angkon Kar <angkon310@gmail.com>
2025-12-21 20:58:18 +0600

    Recursion problem

---
+++ Recursion/06.cpp
@@ -0,0 +1,25 @@
+// Given 2 integers a and b,(1 <= a,b <= 10), print a^b using recursion.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int power(int a, int b){
+    //base case
+    if(b==0) return 1;
+    
+    //recursive case
+    return a * power(a, b-1);
+}
+
+int main() {
+    int a = 2, b = 5;
+    cout << a << "^" << b << " = " << power(a, b) << endl; //2^5 = 32
+
+    a = 3, b = 4;
+    cout << a << "^" << b << " = " << power(a, b) << endl; //3^4 = 81
+
+    a = 5, b = 3;
+    cout << a << "^" << b << " = " << power(a, b) << endl; //5^3 = 125
+
+    return 0;
+}
