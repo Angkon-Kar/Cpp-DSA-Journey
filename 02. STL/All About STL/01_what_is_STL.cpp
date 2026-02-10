e95d20e4645a2cd13da1ec915667a7db35914b38
Angkon Kar <angkon310@gmail.com>
2026-02-10 14:36:17 +0600

    stl update

--- 02. STL/All About STL/01_what_is_STL.cpp
+++ 02. STL/All About STL/01_what_is_STL.cpp
@@ -1,23 +1,17 @@
-//  What is STL?
-
-// The C++ Standard Template Library
-// Douglas C. Schmidt
-// Vanderbilt University
-// February 12, 2014
-//
-// What is STL?
-// The Standard Template Library provides a set of well-structured generic C++ components that work together in a seamless way.
-// - Alexander Stepanov & Meng Lee, The Standard Template Library
-//
-// What is STL (cont'd)?
-// • A collection of composable class & function templates
-//   - Helper class & function templates: operators, pair
-//   - Container & iterator class templates
-//   - Generic algorithms that operate over iterators
-//   - Function objects
-//   - Adaptors
-// • Enables generic programming in C++
-//   - Each generic algorithm can operate over any iterator for which the necessary operations are provided
-//   - Extensible: can support new algorithms, containers, iterators
-
-// No executable code for this introductory file.
+/*
+ * File: 01_Intro_to_STL.cpp
+ * ------------------------
+ * The Standard Template Library (STL) is a powerful set of C++ template classes 
+ * that provide ready-to-use data structures and algorithms.
+ *
+ * It consists of four main components:
+ * 1. Containers: Used to manage collections of objects (e.g., vector, list, map).
+ * 2. Algorithms: Act on containers (e.g., sort, search, transform).
+ * 3. Iterators: The bridge between algorithms and containers.
+ * 4. Functors: Objects that can be treated as functions.
+ *
+ * Why use STL?
+ * - Efficiency: Highly optimized by compiler experts.
+ * - Safety: Reduces the risk of manual memory management errors.
+ * - Reusability: Generic code that works with any data type.
+ */
