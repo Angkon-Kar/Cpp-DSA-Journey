b60e39908876085d01d058295734b0c979f09c76
Angkon Kar <angkon310@gmail.com>
2026-03-22 01:44:34 +0600

    MY learning Codes -> MSC

---
+++ 02. STL/MSC/05.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int>v;
+
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(5);
+    v.push_back(7);
+    v.push_back(11);
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << ' ';
+    }
+    cout << endl;
+    
+    return 0;
+}
+// output:
+// 2 3 5 7 11
