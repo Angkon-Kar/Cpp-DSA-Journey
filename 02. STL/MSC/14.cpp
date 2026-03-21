f262b7a1e7c0c09dc483ca8e41e78dbc5b298785
Angkon Kar <angkon310@gmail.com>
2026-03-22 02:07:04 +0600

    MY learning Codes -> MSC

--- 02. STL/MSC/14.cpp
+++ 02. STL/MSC/14.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main() {
     vector<int>v(5, 10); // creates a vector of size 5 with all elements initialized to 10
-    v.assign(3,11); // assigns 3 elements with value 11
+    v.resize(3); // it resizes the vector to size 3, it removes the last 2 elements of the vector
     cout << v.size() << endl; // output: 3
     for (int i = 0; i < v.size(); i++)
     {
-        cout << v[i] << ' '; // output: 11 11 11
+        cout << v[i] << ' '; // output: 10 10 10
     }
     
     return 0;
