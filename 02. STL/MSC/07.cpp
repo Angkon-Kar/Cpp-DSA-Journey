444212f8abbb5b734064c02b74babebf38b82b8c
Angkon Kar <angkon310@gmail.com>
2026-03-22 01:51:15 +0600

    MY learning Codes -> MSC

--- 02. STL/MSC/07.cpp
+++ 02. STL/MSC/07.cpp
@@ -2,16 +2,7 @@
 using namespace std;
 
 int main() {
-    vector<int>v;
-
-    v.push_back(2);
-    v.push_back(3);
-    v.push_back(5);
-    v.push_back(7);
-    v.push_back(11);
-    v.push_back(13);
-    v.pop_back(); // it removes the last element of the vector = 13
-    v.pop_back(); // it removes the last element of the vector = 11
-    
-    cout << v.back() << endl; // output: 7
+    vector<int>v(5); // creates a vector of size 5 with all elements initialized to 0
+    cout<<v.size()<<endl; // output: 5
+    return 0;
 }
