288826ba615d3bf96fa749eb24ad0a011904846a
Angkon Kar <angkon310@gmail.com>
2026-03-22 01:41:57 +0600

    MY learning Codes -> MSC

---
+++ 02. STL/MSC/03.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int>v;
+
+    v.push_back(1);
+    v.push_back(2);
+    cout << v[0] << ' ' << v[1] << endl; // output: 1 2
+    cout << v.size() << endl; // output: 2
+    
+    return 0;
+}
+
+// output:
+// 1 2
+// 2
