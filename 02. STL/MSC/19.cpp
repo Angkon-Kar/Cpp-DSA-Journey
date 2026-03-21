76f4ede7cbf9635367f11dc3d68585c597cbfb47
Angkon Kar <angkon310@gmail.com>
2026-03-22 02:46:29 +0600

    MY learning Codes -> MSC

--- 02. STL/MSC/19.cpp
+++ 02. STL/MSC/19.cpp
@@ -3,7 +3,12 @@ using namespace std;
 
 int main() {
     vector<int>v{1,2,3,4,5};
-    v.erase(v.begin() + 1); // it removes the element at index 2 which is 3
+    v.erase(v.begin() + 1); // it removes the element at index 1 which is 2
+    cout << v.size() << endl; // output: 4
+    for (auto &x : v)
+    {
+        cout << x << ' '; // output: 1 3 4 5
+    }
 
 
 return 0;
